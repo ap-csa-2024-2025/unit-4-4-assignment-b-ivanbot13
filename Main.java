693bc596e8a9e943e794ec8cb9a67f129c015212
Ivan <ikduong100@student.hbuhsd.edu>
2024-11-13 19:04:31 +0000

    uno

--- Main.java
+++ Main.java
@@ -4,12 +4,31 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Put your solutions below
+    Scanner sc = new Scanner(System.in);
+
+    //prob 1:
+    System.out.println("Input word:");
+    String word = sc.nextLine();
+    System.out.println("Input target:");
+    String target = sc.nextLine();
+    System.out.println("The substring " + target + " appears " + countSub(word, target) + " times");
+
+    //prob 2:
+    
   }
 
   public static int countSub(String word, String target)
   {
-    return 0;
+    int count = 0;
+    word = word.toLowerCase();
+    target = target.toLowerCase();
+    for (int i = 0; i < word.length()-(target.length()-1); i++){
+      String check = word.substring(i, i + target.length());
+      if (check.equals(target)){
+        count++;
+      }
+    }
+    return count;
   }
 
   public static int countProperContains(String word, String target)
