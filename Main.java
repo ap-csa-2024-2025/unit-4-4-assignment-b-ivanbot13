2491c15cf7c153671aa4afb5ac7b9c6923ac1f43
Ivan <ikduong100@student.hbuhsd.edu>
2024-11-14 19:18:45 +0000

    debug2

--- Main.java
+++ Main.java
@@ -7,14 +7,18 @@ public class Main
     Scanner sc = new Scanner(System.in);
 
     //prob 1:
-    System.out.println("Input word:");
-    String word = sc.nextLine();
-    System.out.println("Input target:");
-    String target = sc.nextLine();
-    System.out.println("The substring " + target + " appears " + countSub(word, target) + " times");
+    // System.out.println("Input word:");
+    // String word = sc.nextLine();
+    // System.out.println("Input target:");
+    // String target = sc.nextLine();
+    // System.out.println("The substring " + target + " appears " + countProperContains(word, target) + " times");
 
     //prob 2:
-    
+    System.out.println("Input word:");
+    String word2 = sc.nextLine();
+    System.out.println("Input target:");
+    String target2 = sc.nextLine();
+    System.out.println("The substring " + target2 + " is properly contained " + countSub(word2, target2) + " times");
   }
 
   public static int countSub(String word, String target)
@@ -22,7 +26,7 @@ public class Main
     int count = 0;
     word = word.toLowerCase();
     target = target.toLowerCase();
-    for (int i = 0; i < word.length()-(target.length()-1); i++){
+    for (int i = 0; i < word.length() - (target.length()-1); i++){
       String check = word.substring(i, i + target.length());
       if (check.equals(target)){
         count++;
@@ -33,7 +37,21 @@ public class Main
 
   public static int countProperContains(String word, String target)
   {
-    return 0;
+    int count = 0;
+    for (int i = 0; i > word.length() - (target.length()-1); i++){
+      int beginOfTar = i;
+      boolean beginOrSpaceB4 = (beginOfTar == 0 || word.substring(beginOfTar-1, beginOfTar).equals(" "));
+
+      int endOfTar = i + target.length();
+      boolean endOrSpaceAfter = (endOfTar == word.length() || word.substring(endOfTar, endOfTar+1).equals(" "));
+      boolean propContain = beginOrSpaceB4 && endOrSpaceAfter;
+
+      String sub = word.substring(i, i + target.length());
+      if (sub.equals(target) && propContain){
+        count++;
+      }
+    }
+    return count;
   }
 
   public static String deleteSub(String word, String target)
